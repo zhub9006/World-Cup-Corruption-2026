0c6b62a6c9bff6bc72f7c2de157b8c714b16776d
FadyEmad01 <fadyemad933@gmail.com>
2026-07-08 10:18:30 +0300

    init the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,9 +28,8 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className={`min-h-screen flex flex-col antialiased ${geistSans.variable} ${geistMono.variable} ${inter.variable}`}>{children}</body>
     </html>
   );
 }
